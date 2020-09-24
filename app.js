ac557905b8a93ae030290550b9f89237002e7430
Ryan Durk <rpdurk@gmail.com>
2020-09-24 15:53:55 -0700

    js constructors have been added

--- app.js
+++ app.js
@@ -33,3 +33,55 @@ const render = require("./lib/htmlRenderer");
 // for further information. Be sure to test out each class and verify it generates an
 // object with the correct structure and methods. This structure will be crucial in order
 // for the provided `render` function to work! ```
+const questions = [
+    {
+        type: 'input',
+        name: 'email',
+        message: 'Please enter your email.',
+    },
+    {
+        type: 'input',
+        name: 'id',
+        message: 'What is your employee id?.',
+    },
+    {
+        type: "list",
+        name: "status",
+        message: "What is your role?",
+        choices: [
+            "Intern",
+            "Engineer",
+            "Manager",
+        ]
+    },
+];
+
+// prompt user the above questions. These will be incorporated into the README
+const prompt = function() {
+    // must return the answers from the user, to be utilized in the README
+    return inquirer.prompt(questions)
+};
+
+// initiate user information and README creation.
+const init = async function(){
+    // attempt to do the following
+    try {
+        // wait for the prompt function to be ran
+        const userAnswers = await prompt()
+        // console.log(userAnswers);
+        // utilize the prompt function to create a response including the username
+        username = userAnswers.username;
+        // console.log(username);
+        // create a variable to hold the api call function in -this will return the github data
+        const response = await api(username);
+        // console.log("inside init index in 84", response);
+        // generate readMe by passing in prompt()answers and api response
+        const writeReadMe = generateReadMe(userAnswers, response);
+        await writeFileAsync("README.MD", writeReadMe);
+        console.log("README successfully generated.")
+    } catch (err){
+        console.log(err)
+    }
+};
+
+init();
