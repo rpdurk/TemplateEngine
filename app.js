3edf3b2af52f2db69169d99074b1f823237b5e9c
Ryan Durk <rpdurk@gmail.com>
2020-09-25 12:05:43 -0700

    function to prompt command line questions and updated questions array

--- app.js
+++ app.js
@@ -1,39 +1,33 @@
+// require manager js
 const Manager = require("./lib/Manager");
+// require engineer js
 const Engineer = require("./lib/Engineer");
+// require intern js
 const Intern = require("./lib/Intern");
+// require inquirer
 const inquirer = require("inquirer");
+// require path
 const path = require("path");
+// require fs for write file
 const fs = require("fs");
 
+// 
 const OUTPUT_DIR = path.resolve(__dirname, "output");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
 
+// require the htmlRenderer js to add each person's info to respective html
 const render = require("./lib/htmlRenderer");
+// empty array for team member objects to be pushed
+const teamMembers = [];
 
-
-// Write code to use inquirer to gather information about the development team members,
-// and to create objects for each team member (using the correct classes as blueprints!)
-
-// After the user has input all employees desired, call the `render` function (required
-// above) and pass in an array containing all employee objects; the `render` function will
-// generate and return a block of HTML including templated divs for each employee!
-
-// After you have your html, you're now ready to create an HTML file using the HTML
-// returned from the `render` function. Now write it to a file named `team.html` in the
-// `output` folder. You can use the variable `outputPath` above target this location.
-// Hint: you may need to check if the `output` folder exists and create it if it
-// does not.
-
-// HINT: each employee type (manager, engineer, or intern) has slightly different
-// information; write your code to ask different questions via inquirer depending on
-// employee type.
-
-// HINT: make sure to build out your classes first! Remember that your Manager, Engineer,
-// and Intern classes should all extend from a class named Employee; see the directions
-// for further information. Be sure to test out each class and verify it generates an
-// object with the correct structure and methods. This structure will be crucial in order
-// for the provided `render` function to work! ```
-const questions = [
+// Code to use inquirer to gather information about the team members,
+// and to create objects for each team member (Note to Self: use the correct classes as blueprints)
+const employeeQuestions = [
+    {
+        type: 'input',
+        name: 'name',
+        message: 'Please enter your full name.',
+    },
     {
         type: 'input',
         name: 'email',
@@ -42,11 +36,11 @@ const questions = [
     {
         type: 'input',
         name: 'id',
-        message: 'What is your employee id?.',
+        message: 'What is your employee id?',
     },
     {
         type: "list",
-        name: "status",
+        name: "role",
         message: "What is your role?",
         choices: [
             "Intern",
@@ -54,32 +48,59 @@ const questions = [
             "Manager",
         ]
     },
-];
+]
 
-// prompt user the above questions. These will be incorporated into the README
-const prompt = function() {
-    // must return the answers from the user, to be utilized in the README
-    return inquirer.prompt(questions)
-};
+const engineerQuestions = [
+    {
+        type: 'input',
+        name: 'github',
+        message: 'What is your Github username?',
+    },
+]
 
-// initiate user information and README creation.
-const init = async function(){
-    // attempt to do the following
+const internQuestions = [
+    {
+        type: 'input',
+        name: 'school',
+        message: 'What school are you currently attending?',
+    },
+]
+
+const managerQuestions = [
+    {
+        type: 'input',
+        name: 'office',
+        message: 'What is your office number?',
+    },
+]
+
+const addEmployeeQuestions = [
+    {
+        type: "confirm",
+        name: "addEmployee",
+        message: "Who you like to add another employee?",
+    },
+]
+
+// function that adds employees to array based on type
+const init = async () => {
+    // uses a try because async function
     try {
-        // wait for the prompt function to be ran
-        const userAnswers = await prompt()
-        // console.log(userAnswers);
-        // utilize the prompt function to create a response including the username
-        username = userAnswers.username;
-        // console.log(username);
-        // create a variable to hold the api call function in -this will return the github data
-        const response = await api(username);
-        // console.log("inside init index in 84", response);
-        // generate readMe by passing in prompt()answers and api response
-        const writeReadMe = generateReadMe(userAnswers, response);
-        await writeFileAsync("README.MD", writeReadMe);
-        console.log("README successfully generated.")
-    } catch (err){
+        // prompt standard set of employee questions + role
+        const employeeAnswers = await inquirer.prompt(employeeQuestions);
+        // if the answer of role in the role function is manager, do the following
+        if (employeeAnswers.role === "Manager") {
+            // create a variable to store the answers to manager questions 
+            const managerAnswers = await inquirer.prompt(managerQuestions);
+            // create a variable for manager that stores constructor by answers
+            const manager = new Manager (employeeAnswers.name, employeeAnswers.email, employeeAnswers.id, managerAnswers.office);
+            // push variable to array as an object
+            console.log('init manager object', manager)
+            teamMembers.push(manager);
+            addAnotherEmployee();
+        } else if  (employeeAnswers.role === "Engineer") {
+        } else { (employeeAnswers.role === "Intern") 
+    } catch (err) {
         console.log(err)
     }
 };
